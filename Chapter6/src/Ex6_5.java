37dfe5663312d2d5a7240e55aafd37de071c2c44
SHIN <zm255@naver.com>
2022-05-23 22:59:30 +0900

    day220523_1

--- Chapter6/src/Ex6_5.java
+++ Chapter6/src/Ex6_5.java
@@ -1,11 +1,11 @@
-class CartTest{
-public class Ex6_5 {
-//Card Test
-	public static void main(String[] args) {
-		System.out.println("");
-		
-		
-	}
-
-}
-}
+//class CartTest{
+//public class Ex6_5 {
+////Card Test
+//	public static void main(String[] args) {
+//		System.out.println("");
+//		
+//		
+//	}
+//
+//}
+//}
