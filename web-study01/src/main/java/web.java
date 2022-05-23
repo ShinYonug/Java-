a51f821ddb2ad7f151ae7a40ff85ef47abaf10e5
zm255 <zm255@naver.com>
2022-05-23 15:19:49 +0900

    day220523

---
+++ web-study01/src/main/java/web.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class web
+ */
+@WebServlet("/web")
+public class web extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public web() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	int num1 = 20;
+	int num2 = 10;
+	int add = num1 + num2;
+	PrintWriter out = response.getWriter()	;
+	out.println("<html><head></head></html>");
+	out.println("<body>");
+	out.println(num1 + " + " + num2);
+	out.println("</body>");
+	out.println("</html>");
+	System.out.println("num1 + num2 =" + add);
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
